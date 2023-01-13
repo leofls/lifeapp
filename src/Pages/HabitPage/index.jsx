bf4760803b1699eab3020e85e1373df9222bf553
Leandro F <leandro@licitegov.com.br>
2023-01-13 11:18:45 -0300

    create Habit e estrutura de rotas

--- src/Pages/HabitPage/index.jsx
+++ src/Pages/HabitPage/index.jsx
@@ -1,52 +1,87 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import {
-    View,
-    Text,
-    StyleSheet,
-    Image,
-    TouchableOpacity,
-    ScrollView,
-    Alert
-} from 'react-native'
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+} from "react-native";
 
-export default function HabitPage() {
-    const navigation = useNavigation();
-
-    return (
-        <View style={styles.container}>
-            <ScrollView >
-                <View style={{ alignItems: "center" }}>
-                    <TouchableOpacity 
-                        style={styles.backPageBtn} 
-                        onPress={() => navigation.goBack()}
-                    >
-                        <Image 
-                            source={require("../../assets/icons/arrowBack.png")}
-                            style={styles.arrowBack}
-                        ></Image>
-                    </TouchableOpacity>
-                    <View style={styles.mainContent}></View>
-                </View>
-            </ScrollView>
+export default function HabitPage({ route }) {
+  const navigation = useNavigation();
+  const { create, habit } = route.params;
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View>
+          <TouchableOpacity
+            style={styles.bakcPageBtn}
+            onPress={() => navigation.goBack()}
+          >
+            <Image
+              source={require("../../assets/icons/arrowBack.png")}
+              style={styles.arrowBack}
+            ></Image>
+          </TouchableOpacity>
+          <View style={styles.mainContent}>
+            <Text style={styles.title}>Configurações {"\n"} de hábito</Text>
+            <Text style={styles.inputText}>Área</Text>
+            <View style={styles.inputContainer}>
+              <Text style={styles.area}>{habit?.habitArea}</Text>
+            </View>
+          </View>
         </View>
-    )
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: 40,
-        height: 40,
-        margin: 25,
-    },
-    arrowBack: {
-        width: 40,
-        height: 40,
-    },
-    mainContent: {
-        width: 250,
-        alignSelf:"center"
-    }
-
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  bakcPageBtn: {
+    width: 40,
+    height: 40,
+    margin: 25,
+  },
+  arrowBack: {
+    width: 40,
+    height: 40,
+  },
+  mainContent: {
+    width: 250,
+    alignSelf: "center",
+  },
+  configButton: {
+    alignItems: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    color: "white",
+    fontSize: 30,
+  },
+  inputText: {
+    color: "white",
+    fontSize: 16,
+    marginTop: 35,
+    marginBottom: 10,
+    marginLeft: 5,
+  },
+  inputContainer: {
+    borderWidth: 1,
+    borderColor: "#FFFFFF",
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 15,
+  },
+  area: {
+    color: "#BBBBBB",
+    fontSize: 15,
+  },
 });
